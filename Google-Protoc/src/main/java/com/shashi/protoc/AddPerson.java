bcb588fa399d635d0840d475f73cec356259b474
Shashi Bhushan <shashib@intelligrape.com>
2015-12-29 16:29:47 +0530

    Protoc - Added AddPerson protoc class

---
+++ Google-Protoc/src/main/java/com/shashi/protoc/AddPerson.java
@@ -0,0 +1,73 @@
+package com.shashi.protoc;
+
+import com.shashi.protoc.generated.AddressBookProtos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+/**
+ * @author Shashi Bhushan
+ *         Created on 29/12/15.
+ *         For Google-Protoc
+ */
+public class AddPerson {
+
+    /**
+     * Fills in a Person Message based on User Input
+     * @return
+     */
+    public static AddressBookProtos.Person promptForAddress(BufferedReader stdin,
+                                                            PrintStream stdout) throws IOException{
+
+        AddressBookProtos.Person.Builder personBuilder = AddressBookProtos.Person.newBuilder();
+
+        stdout.print("Enter Person ID:");
+        personBuilder.setId(Integer.valueOf(stdin.readLine()));
+
+        stdout.print("Enter Name:");
+        personBuilder.setName(stdin.readLine());
+
+        stdout.print("Enter Email Address (blank for none):");
+        String email = stdin.readLine();
+
+        if(email.length() > 0){
+            personBuilder.setEmail(email);
+        }
+
+        while(true){
+            stdout.print("Enter a Phone Number (or leave blank to finish):");
+
+            String number = stdin.readLine();
+
+            if(number.length() == 0){
+                break;
+            }
+
+            AddressBookProtos.Person.PhoneNumber.Builder phoneNumberBuilder =
+                    AddressBookProtos.Person.PhoneNumber.newBuilder().setNumber(number);
+
+            stdout.print("is this a mobile, home or work phone?");
+
+            String type = stdin.readLine();
+
+            switch(type){
+                case "mobile":
+                    phoneNumberBuilder.setType(AddressBookProtos.Person.PhoneType.MOBILE);
+                    break;
+                case "home":
+                    phoneNumberBuilder.setType(AddressBookProtos.Person.PhoneType.HOME);
+                    break;
+                case "work":
+                    phoneNumberBuilder.setType(AddressBookProtos.Person.PhoneType.WORK);
+                    break;
+                default:
+                    stdout.print("Unknown Phone Type. Using Default.");
+            }
+
+            personBuilder.addNumber(phoneNumberBuilder);
+        }
+
+        return personBuilder.build();
+    }
+}
