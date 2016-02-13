599e6f9e05705f11f751e73ebee82f67fe5a895b
Shashi Bhushan <shashib@intelligrape.com>
2016-02-13 20:19:18 +0530

    HeadFirstJava - ToDo

---
+++ ToDo.java
@@ -0,0 +1,8 @@
+public class ToDo{
+	/*
+	 * ToDO:
+	 * 
+	 * Ask on SO 
+	 * 1. if list.size() can return only Integer.MAX_VALUE, then if a List has more then Integer.MAX_VALUE, efficient way to get length of such ArrayList.
+	 */ 
+}
