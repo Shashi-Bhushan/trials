2a8f37a086d24fbb819f00f9b6c7675c5c65ee7b
Shashi <shashi@vayana.com>
2020-04-29 01:53:29 +0530

    Find Missing in O(n) time and O(k) space

---
+++ algo-trials/src/main/java/in/shabhushan/algo_trials/trials/MissingElements.java
@@ -0,0 +1,31 @@
+package in.shabhushan.algo_trials.trials;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MissingElements {
+  public static List<Integer> missingElements(int[] nums, int k) {
+    int[] numbers = new int[nums.length + k];
+
+    for (int i = 0; i < numbers.length; i++) {
+      if (i < nums.length) {
+        numbers[i] = nums[i];
+      } else {
+        numbers[i] = nums[0];
+      }
+    }
+
+    // Sort numbers in their range only upto n, not n + k
+    for (int i = 0; i < nums.length; i++) {
+      while (numbers[numbers[i]] != numbers[i]) {
+        int temp = numbers[numbers[i]];
+        numbers[numbers[i]] = numbers[i];
+        numbers[i] = temp;
+      }
+    }
+
+    // Return indexes
+    return IntStream.range(0, numbers.length).filter(i -> numbers[i] != i).boxed().collect(Collectors.toList());
+  }
+}
