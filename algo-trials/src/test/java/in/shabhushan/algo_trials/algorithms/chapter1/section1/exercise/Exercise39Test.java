ab02cc783aee4a3673634f7d852816bc88aaddd1
Shashi Bhushan <sbhushan1@outlook.com>
2020-05-09 04:01:04 +0530

    Exercise 39 Test refactoring

--- algo-trials/src/test/java/in/shabhushan/algo_trials/algorithms/chapter1/section1/exercise/Exercise39Test.java
+++ algo-trials/src/test/java/in/shabhushan/algo_trials/algorithms/chapter1/section1/exercise/Exercise39Test.java
@@ -9,13 +9,13 @@ public class Exercise39Test {
   @Test
   public void test() {
 
-    int[] results = mainExperiment(10);
+    int[] results = mainExperiment(30);
 
     StdOut.println("Results");
 
-    StdOut.printf("10ˆ3: %7.2f \n", ((double)results[0]) / 10);
-    StdOut.printf("10ˆ4: %7.2f \n", ((double)results[1]) / 10);
-    StdOut.printf("10ˆ5: %7.2f \n", ((double)results[2]) / 10);
-    StdOut.printf("10ˆ6: %7.2f \n", ((double)results[3]) / 10);
+    StdOut.printf("10ˆ3: %7.4f \n", ((double)results[0]) / 10);
+    StdOut.printf("10ˆ4: %7.4f \n", ((double)results[1]) / 10);
+    StdOut.printf("10ˆ5: %7.4f \n", ((double)results[2]) / 10);
+    StdOut.printf("10ˆ6: %7.4f \n", ((double)results[3]) / 10);
   }
 }
