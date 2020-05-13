9030f3f7e4c2b480808ff72b4c75f41a5afb70ea
Shashi <sbhushan1@outlook.com>
2020-05-14 02:24:43 +0530

    Leetcode Remove k digits solution added

---
+++ cp-trials/src/main/java/in/shabhushan/cp_trials/stack/RemoveKDigits.java
@@ -0,0 +1,38 @@
+package in.shabhushan.cp_trials.stack;
+
+import java.util.*;
+
+/**
+ * Leetcode solution for
+ * https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3328/
+ *
+ * Check https://www.youtube.com/watch?v=BbNS_sKBAqo for reference
+ */
+class RemoveKDigits {
+  public static String removeKdigits(String num, int k) {
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+    for (char c: num.toCharArray()) {
+      int x = c - '0';
+      while (k > 0 && !stack.isEmpty() && stack.peek() > x) {
+        stack.pop();
+        k--;
+      }
+
+      if (stack.size() == 0 && x == 0) {} else
+        stack.push(x);
+    }
+
+    while (k-- > 0 && !stack.isEmpty())
+      stack.pop();
+
+    StringBuilder sb = new StringBuilder();
+
+    while (!stack.isEmpty()) {
+      sb.append(String.valueOf(stack.pop()));
+    }
+
+    sb.reverse();
+    return (sb.length() == 0) ? "0" : sb.toString();
+  }
+}
---
+++ cp-trials/src/test/java/in/shabhushan/cp_trials/stack/RemoveKDigitsTest.java
@@ -0,0 +1,18 @@
+package in.shabhushan.cp_trials.stack;
+
+import org.junit.Test;
+
+import static in.shabhushan.cp_trials.stack.RemoveKDigits.removeKdigits;
+import static org.junit.Assert.assertEquals;
+
+public class RemoveKDigitsTest {
+  @Test
+  public void removeKDigits() {
+    assertEquals("1219", removeKdigits("1432219", 3));
+    assertEquals("200", removeKdigits("10200", 1));
+    assertEquals("0", removeKdigits("10", 2));
+    assertEquals("523000", removeKdigits("12345000000523000", 5));
+    assertEquals("23000", removeKdigits("12345000000523000", 6));
+    assertEquals("0", removeKdigits("9", 1));
+  }
+}
