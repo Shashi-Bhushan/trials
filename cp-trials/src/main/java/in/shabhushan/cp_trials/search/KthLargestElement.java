36f30845aaf8ce5b99f7a2b4b7d08dff89d11d01
Shashi <sbhushan1@outlook.com>
2020-05-18 20:31:11 +0530

    Find kth largest element solution added

--- cp-trials/src/main/java/in/shabhushan/cp_trials/search/KthLargestElement.java
+++ cp-trials/src/main/java/in/shabhushan/cp_trials/search/KthLargestElement.java
@@ -1,11 +1,60 @@
 package in.shabhushan.cp_trials.search;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class KthLargestElement {
+  /**
+   * O(NlogN)
+   */
   public static int findKthLargest(int[] nums, int k) {
     Arrays.sort(nums);
 
     return nums[nums.length - k];
+  }
+
+  /**
+   * O(NlogK)
+   */
+  public static int findKthLargest2(int[] nums, int k) {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a - b);
+
+    for (int num: nums) {
+      minHeap.add(num);
+
+      if (minHeap.size() > k) {
+        minHeap.poll();
+      }
+    }
+
+    return minHeap.poll();
+  }
+
+  public static int findKthLargest3(int[] nums, int k) {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+
+    for (int num: nums) {
+      min = Math.min(min, num);
+      max = Math.max(max, num);
+    }
+
+    int[] bucket = new int[max - min + 1];
+
+    for (int num: nums) {
+      bucket[num - min]++;
+    }
+
+    int m = 0;
+    for (int i = bucket.length - 1; i >= 0; i--) {
+      m += bucket[i];
+
+      if (m >= k) {
+        return i + min;
+      }
+    }
+
+    return -1;
   }
 }
--- cp-trials/src/test/java/in/shabhushan/cp_trials/search/KthLargestElementTest.java
+++ cp-trials/src/test/java/in/shabhushan/cp_trials/search/KthLargestElementTest.java
@@ -2,14 +2,29 @@ package in.shabhushan.cp_trials.search;
 
 import org.junit.Test;
 
-import static in.shabhushan.cp_trials.search.KthLargestElement.findKthLargest;
+import static in.shabhushan.cp_trials.search.KthLargestElement.*;
 import static org.junit.Assert.assertEquals;
 
 public class KthLargestElementTest {
   @Test
   public void testKthLargestElement() {
-    assertEquals(6, findKthLargest(new int[]{3,2,1,5,6,4}, 1));
-    assertEquals(5, findKthLargest(new int[]{3,2,1,5,6,4}, 2));
-    assertEquals(4, findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 4));
+    assertEquals(6, findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 1));
+    assertEquals(5, findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+    assertEquals(4, findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+  }
+
+  @Test
+  public void testKthLargestElement2() {
+    assertEquals(4, findKthLargest2(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+    assertEquals(6, findKthLargest2(new int[]{3, 2, 1, 5, 6, 4}, 1));
+    assertEquals(5, findKthLargest2(new int[]{3, 2, 1, 5, 6, 4}, 2));
+  }
+
+  @Test
+  public void testKthLargestElement3() {
+    assertEquals(6, findKthLargest3(new int[]{3, 2, 5, 6, 4, 7, 8, 10, 9, 12, 11, 15,14,13, 20,19,18,17,16}, 2));
+    assertEquals(5, findKthLargest3(new int[]{3, 2, 1, 5, 6, 4}, 2));
+    assertEquals(6, findKthLargest3(new int[]{3, 2, 1, 5, 6, 4}, 1));
+    assertEquals(4, findKthLargest3(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
   }
 }
