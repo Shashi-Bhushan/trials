644d20236997793f3f8b4069b5ea371e381f98bb
Shashi <sbhushan1@outlook.com>
2020-05-24 20:31:52 +0530

    Maximum Number vowels solution added

--- cp-trials/src/main/java/in/shabhushan/cp_trials/prefix_sum/MaximumNumberVowels.java
+++ cp-trials/src/main/java/in/shabhushan/cp_trials/prefix_sum/MaximumNumberVowels.java
@@ -2,6 +2,10 @@ package in.shabhushan.cp_trials.prefix_sum;
 
 import java.util.Set;
 
+/**
+ * Leetcode solution for
+ * https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+ */
 public class MaximumNumberVowels {
   private static Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
 
@@ -15,6 +19,35 @@ public class MaximumNumberVowels {
     int max = 0;
     for (int i = k; i <= s.length(); i++) {
       max = Math.max(max, rank[i] - rank[i - k]);
+    }
+
+    return max;
+  }
+
+  public static int maxVowels2(String s, int k) {
+    int t = 0;
+    int max = 0;
+
+    for (int i = 0; i < k; i++) {
+      if (vowels.contains(s.charAt(i))) {
+        t++;
+      }
+    }
+
+    int last = 0;
+    max = Math.max(max, t);
+
+    for (int i = k; i < s.length(); i++) {
+      if (vowels.contains(s.charAt(last))) {
+        t--;
+      }
+
+      if (vowels.contains(s.charAt(i))) {
+        t++;
+      }
+
+      last++;
+      max = Math.max(max, t);
     }
 
     return max;
--- cp-trials/src/test/java/in/shabhushan/cp_trials/prefix_sum/MaximumNumberVowelsTest.java
+++ cp-trials/src/test/java/in/shabhushan/cp_trials/prefix_sum/MaximumNumberVowelsTest.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static in.shabhushan.cp_trials.prefix_sum.MaximumNumberVowels.maxVowels;
+import static in.shabhushan.cp_trials.prefix_sum.MaximumNumberVowels.maxVowels2;
 import static org.junit.Assert.assertEquals;
 
 public class MaximumNumberVowelsTest {
@@ -25,5 +25,15 @@ public class MaximumNumberVowelsTest {
     assertEquals(0, maxVowels("rhythms", 4));
     assertEquals(1, maxVowels("tryhard", 4));
     assertEquals(84, maxVowels(longString, 316));
+  }
+
+  @Test
+  public void test2() {
+    assertEquals(3, maxVowels2("abciiidef", 3));
+    assertEquals(2, maxVowels2("aeiou", 2));
+    assertEquals(2, maxVowels2("leetcode", 3));
+    assertEquals(0, maxVowels2("rhythms", 4));
+    assertEquals(1, maxVowels2("tryhard", 4));
+    assertEquals(84, maxVowels2(longString, 316));
   }
 }
