ffcf979bb8e85c17fe8de24e47a349ad82ba88cc
Shashi <sbhushan1@outlook.com>
2020-05-27 12:14:31 +0530

    Contiguous array problem statement updated

--- cp-trials/src/main/java/in/shabhushan/cp_trials/array/ContiguousArray.java
+++ cp-trials/src/main/java/in/shabhushan/cp_trials/array/ContiguousArray.java
@@ -2,6 +2,12 @@ package in.shabhushan.cp_trials.array;
 
 import java.util.*;
 
+/**
+ * Java solution for
+ * https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3341/
+ *
+ * Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+ */
 class ContiguousArray {
   public static int findMaxLength(int[] nums) {
     Map<Integer, Integer> map = new HashMap<>();
