5a103c54a60fb700fd5e8a92f0ea6df412e4a15f
Shashi <sbhushan1@outlook.com>
2020-05-30 02:50:53 +0530

    Min cost of travel between stations iterative solution

--- algo-trials/src/main/java/in/shabhushan/algo_trials/dynamic_programming/chapter4/MinimumCostOfTravel.java
+++ algo-trials/src/main/java/in/shabhushan/algo_trials/dynamic_programming/chapter4/MinimumCostOfTravel.java
@@ -13,4 +13,22 @@ public class MinimumCostOfTravel {
 
     return min;
   }
+
+  public static int getMinimumCostFaster(int[][] cost) {
+    int[] minCost = new int[cost.length];
+
+    minCost[0] = 0;
+    minCost[1] = cost[0][1];
+
+    for (int i = 2; i < cost.length; i++) {
+      minCost[i] = cost[0][i];
+
+      // cost between 0 and i, considering j as middle station
+      for (int j = 1; j < i; j++) {
+        minCost[i] = Math.min(minCost[i], minCost[j] + cost[j][i]);
+      }
+    }
+
+    return minCost[cost.length - 1];
+  }
 }
--- algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/chapter4/MinCostBetweenStationsTest.java
+++ algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/chapter4/MinCostBetweenStationsTest.java
@@ -3,6 +3,7 @@ package in.shabhushan.algo_trials.dynamic_programming.chapter4;
 import org.junit.Test;
 
 import static in.shabhushan.algo_trials.dynamic_programming.chapter4.MinimumCostOfTravel.getMinimumCost;
+import static in.shabhushan.algo_trials.dynamic_programming.chapter4.MinimumCostOfTravel.getMinimumCostFaster;
 import static org.junit.Assert.assertEquals;
 
 public class MinCostBetweenStationsTest {
@@ -17,5 +18,17 @@ public class MinCostBetweenStationsTest {
 
     assertEquals(45, getMinimumCost(costs, 0, 2));
     assertEquals(60, getMinimumCost(costs, 0, 3));
+  }
+
+  @Test
+  public void testMinCostBetweenStationsFaster() {
+    int[][] costs = new int[][]{
+        {0, 10, 75, 94},
+        {-1, 0, 35, 50},
+        {-1, -1, 0, 80},
+        {-1, -1, -1, 0}
+    };
+
+    assertEquals(60, getMinimumCostFaster(costs));
   }
 }
