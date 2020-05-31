ab7bdc5fe64ad5e07aa9a8d5e3b8a802e25353d7
Shashi <sbhushan1@outlook.com>
2020-05-31 21:30:50 +0530

    Count unique ways solution added

--- algo-trials/src/main/java/in/shabhushan/algo_trials/dynamic_programming/chapter8/WaysToScore.java
+++ algo-trials/src/main/java/in/shabhushan/algo_trials/dynamic_programming/chapter8/WaysToScore.java
@@ -1,8 +1,11 @@
 package in.shabhushan.algo_trials.dynamic_programming.chapter8;
 
+import java.util.List;
+
 /**
  * Consider a game where each player can score 3, 5, or 10 points in each score.
- * Calculate the total number of unique ways of scoring a score of N.
+ * Calculate the total number of ways of scoring a score of N.
+ * Note 3, 5 is considered different than 5, 3 here.
  */
 public class WaysToScore {
   public static int getWaysToScore(int n) {
@@ -11,7 +14,10 @@ public class WaysToScore {
     else return getWaysToScore(n - 3) + getWaysToScore(n - 5) + getWaysToScore(n - 10);
   }
 
-  public static int getWaysToScoreIterative(int n) {
+  /**
+   * Note how negative condition n < 0 is handled in dp version of solution
+   */
+  public static int getWaysToScoreDP(int n) {
     int[] dp = new int[n + 1];
     dp[0] = 1;
 
@@ -19,6 +25,42 @@ public class WaysToScore {
       if (i - 3 >= 0) dp[i] += dp[i - 3];
       if (i - 5 >= 0) dp[i] += dp[i - 5];
       if (i - 10 >= 0) dp[i] += dp[i - 10];
+    }
+
+    return dp[n];
+  }
+
+  /**
+   * You can find the distinct combinations by enforcing a constraint that
+   * the elements in each combination must be monotonically increasing
+   * https://stackoverflow.com/questions/37850116/dynamic-programming-number-of-distinct-combinations-to-reach-a-given-score
+   */
+  public static int getDistinctWaysToScore(int n, int min) {
+    if (n < 0) return 0;
+    else if (n == 0) return 1;
+    else {
+      int ways = 0;
+
+      if (min <= 3) ways += getDistinctWaysToScore(n - 3, 3);
+      if (min <= 5) ways += getDistinctWaysToScore(n - 5, 5);
+      if (min <= 10) ways += getDistinctWaysToScore(n - 10, 10);
+
+      return ways;
+    }
+  }
+
+  /**
+   * Note how monotonically increasing score has been ensured via a for loop iteration here
+   */
+  public static int getDistinctWaysToScoreDP(int n) {
+    int[] dp = new int[n + 1];
+    dp[0] = 1;
+
+    // for each score, count only ones
+    for (int score : List.of(3, 5, 10)) {
+      for (int i = 1; i <= n; i++) {
+        if (i - score >= 0) dp[i] += dp[i - score];
+      }
     }
 
     return dp[n];
--- algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/chapter8/WaystoScoreTest.java
+++ algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/chapter8/WaystoScoreTest.java
@@ -2,18 +2,31 @@ package in.shabhushan.algo_trials.dynamic_programming.chapter8;
 
 import org.junit.Test;
 
-import static in.shabhushan.algo_trials.dynamic_programming.chapter8.WaysToScore.getWaysToScore;
-import static in.shabhushan.algo_trials.dynamic_programming.chapter8.WaysToScore.getWaysToScoreIterative;
+import static in.shabhushan.algo_trials.dynamic_programming.chapter8.WaysToScore.*;
 import static org.junit.Assert.assertEquals;
 
 public class WaystoScoreTest {
   @Test
   public void testGetWaysToScore() {
+    assertEquals(3, getWaysToScore(11)); // 3 + 3 + 5
     assertEquals(5, getWaysToScore(13));
   }
 
   @Test
   public void testGetWaysToScoreDP() {
-    assertEquals(5, getWaysToScoreIterative(13));
+    assertEquals(3, getWaysToScoreDP(11));
+    assertEquals(5, getWaysToScoreDP(13));
+  }
+
+  @Test
+  public void testGetDistinctWaysToScore() {
+    assertEquals(1, getDistinctWaysToScore(11, 1));
+    assertEquals(2, getDistinctWaysToScore(13, 1));
+  }
+
+  @Test
+  public void testGetDistinctWaysToScoreDP() {
+    assertEquals(1, getDistinctWaysToScoreDP(11));
+    assertEquals(2, getDistinctWaysToScoreDP(13));
   }
 }
