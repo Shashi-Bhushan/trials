43babe32686ce24d0dbc9511f5fe3c1b3a2acbd4
Shashi <sbhushan1@outlook.com>
2020-06-07 04:18:53 +0530

    Subset sum solution added

--- algo-trials/src/main/java/in/shabhushan/algo_trials/dynamic_programming/chapter9/SubsetSum.java
+++ algo-trials/src/main/java/in/shabhushan/algo_trials/dynamic_programming/chapter9/SubsetSum.java
@@ -5,9 +5,6 @@ package in.shabhushan.algo_trials.dynamic_programming.chapter9;
  * array with sum equal to X.
  */
 public class SubsetSum {
-  /**
-   *
-   */
   public static boolean subsetSum(int[] array, int targetSum) {
     return helper(array, 0, 0, targetSum);
   }
@@ -24,5 +21,32 @@ public class SubsetSum {
     }
 
     return false;
+  }
+
+  public static boolean subsetSumDP(int[] array, int targetSum) {
+    boolean[][] dp = new boolean[array.length + 1][targetSum + 1];
+
+    dp[0][0] = true;
+    for (int i = 1; i <= targetSum; i++) {
+      dp[0][i] = false;
+    }
+
+    for (int i = 1; i <= array.length; i++) {
+      dp[i][0] = true;
+    }
+
+    for (int i = 1; i <= array.length; i++) {
+      for (int j = 1; j <= targetSum; j++) {
+        int value = array[i - 1];
+
+        // if j is less than value, copy from previous row
+        if (j < value) dp[i][j] = dp[i - 1][j];
+        // if previous row value is true, set this to true as well.
+        else if (dp[i - 1][j]) dp[i][j] = true;
+        else dp[i][j] = dp[i - 1][j - value];
+      }
+    }
+
+    return dp[array.length][targetSum];
   }
 }
--- algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/chapter9/SubsetSumTest.java
+++ algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/chapter9/SubsetSumTest.java
@@ -3,6 +3,7 @@ package in.shabhushan.algo_trials.dynamic_programming.chapter9;
 import org.junit.Test;
 
 import static in.shabhushan.algo_trials.dynamic_programming.chapter9.SubsetSum.subsetSum;
+import static in.shabhushan.algo_trials.dynamic_programming.chapter9.SubsetSum.subsetSumDP;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -13,5 +14,13 @@ public class SubsetSumTest {
     assertFalse(subsetSum(new int[]{3, 2, 7, 2, 5, 5}, 6));
     assertTrue(subsetSum(new int[]{3, 2, 7, 2, 4, 3, 4, 7, 8}, 10));
     assertFalse(subsetSum(new int[]{3, 2, 7, 2, 4, 3, 4, 7, 8, 3, 2, 7, 2, 4, 3, 4, 7, 8, 3, 2, 7, 2, 4, 3, 4, 7, 8}, 1000));
+  }
+
+  @Test
+  public void testSubsetSumDP() {
+    assertTrue(subsetSumDP(new int[]{3, 2, 7, 1}, 6));
+    assertFalse(subsetSumDP(new int[]{3, 2, 7, 2, 5, 5}, 6));
+    assertTrue(subsetSumDP(new int[]{3, 2, 7, 2, 4, 3, 4, 7, 8}, 10));
+    assertFalse(subsetSumDP(new int[]{3, 2, 7, 2, 4, 3, 4, 7, 8, 3, 2, 7, 2, 4, 3, 4, 7, 8, 3, 2, 7, 2, 4, 3, 4, 7, 8}, 1000));
   }
 }
