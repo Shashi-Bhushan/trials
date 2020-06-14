0c03bb375afb7f4fdf69a3786e71252bd360e683
Shashi <sbhushan1@outlook.com>
2020-06-14 23:39:19 +0530

    Educational DP problem 2 solution added

--- cp-trials/src/main/java/in/shabhushan/cp_trials/competition/atcoder/Main.java
+++ cp-trials/src/main/java/in/shabhushan/cp_trials/competition/atcoder/Main.java
@@ -1,33 +1,146 @@
 package in.shabhushan.cp_trials.competition.atcoder;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
+// Solution
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+  private static Scanner in;
+  private static PrintWriter out;
+  private static final String INPUT = ""; // input file name
 
-    int N = sc.nextInt();
-    int A = sc.nextInt();
-    int B = sc.nextInt();
-    int c = sc.nextInt();
+  /**
+   * Interactive get next command
+   */
+  static int get(int pos) {
+    out.println(pos + 1);
+    out.flush();
+    return ni();
+  }
 
-    List<String> l = new ArrayList<>();
+  static void solve() {
+    int n = ni();
+    int k = ni();
+    int[] arr = new int[n];
 
-    while (N-- > 0) {
-      String s = sc.nextLine();
-
-      if (s.equals("AB")) {
-        // check a or b has max
-        if (A > B) {
-          // subtract from A, Add C to list
-        }
-      } else if (s.equals("AC")) {
-
-      } else {
-
-      }
+    for (int i = 0; i < n; i++) {
+      arr[i] = ni();
     }
+
+    int[] dp = new int[n];
+    dp[0] = 0;
+    dp[1] = Math.abs(arr[1] - arr[0]);
+
+    for (int i = 1; i < n; i++) {
+      int min = Integer.MAX_VALUE;
+
+      for (int j = 1; j <= k; j++) {
+        min = Math.min(min, dp[i - j] + Math.abs(arr[i] - arr[i - j]));
+
+        if (i - j == 0)
+          break;
+      }
+
+      dp[i] = min;
+    }
+
+    out.println(dp[n - 1]);
+  }
+
+  public static void main(String[] args) throws Exception {
+    long S = System.currentTimeMillis();
+    in = INPUT.isEmpty() ? new Scanner(System.in) : new Scanner(INPUT);
+    out = new PrintWriter(System.out);
+
+    solve();
+    out.flush();
+    long G = System.currentTimeMillis();
+    tr(G - S + "ms");
+  }
+
+  private static int gcd(int a, int b) {
+    while (b != 0) {
+      int t = b;
+      b = a % b;
+      a = t;
+    }
+
+    return a;
+  }
+
+  private static int ni() {
+    return Integer.parseInt(in.next());
+  }
+
+  private static long nl() {
+    return Long.parseLong(in.next());
+  }
+
+  // Math Utils
+  private static int pi(int num, int pow) {
+    int result = 1;
+
+    while (pow > 0) {
+      if (pow % 2 == 1) {
+        result *= num;
+      }
+
+      num *= num;
+      pow /= 2;
+    }
+
+    return result;
+  }
+
+  private static int pim(int num, int pow, int mod) {
+    int result = 1;
+
+    while (pow > 0) {
+      if (pow % 2 == 1) {
+        // keep collecting `odd` num in result
+        result = (result * num) % mod;
+      }
+
+      num = (num * num) % mod;
+      pow /= 2;
+    }
+
+    return result;
+  }
+
+  private static long pl(long num, long pow) {
+    long result = 1;
+
+    while (pow > 0) {
+      if (pow % 2 == 1) {
+        result *= num;
+      }
+
+      num *= num;
+      pow /= 2;
+    }
+
+    return result;
+  }
+
+  private static long plm(long num, long pow, long mod) {
+    long result = 1;
+
+    while (pow > 0) {
+      if (pow % 2 == 1) {
+        // keep collecting `odd` num in result
+        result = (result * num) % mod;
+      }
+
+      num = (num * num) % mod;
+      pow /= 2;
+    }
+
+    return result;
+  }
+
+  static void tr(Object... o) {
+    if (!INPUT.isEmpty())
+      out.println(Arrays.deepToString(o));
   }
 }
--- cp-trials/src/main/java/in/shabhushan/cp_trials/dynamic_programming/EducationalDP.java
+++ cp-trials/src/main/java/in/shabhushan/cp_trials/dynamic_programming/EducationalDP.java
@@ -1,71 +1,173 @@
 package in.shabhushan.cp_trials.dynamic_programming;
 
-import java.io.*;
+import java.io.PrintWriter;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
+// Solution
 public class EducationalDP {
-  public static void main(String[] args) {
-    InputStream inputStream = System.in;
-    OutputStream outputStream = System.out;
+  private static Scanner in;
+  private static PrintWriter out;
+  private static final String INPUT = ""; // input file name
 
-    InputReader in = new InputReader(inputStream);
-    PrintWriter out = new PrintWriter(outputStream);
+  /**
+   * Interactive get next command
+   */
+  static int get(int pos) {
+    out.println(pos + 1);
+    out.flush();
+    return ni();
+  }
 
-    int N = in.nextInt();
-    int arr[] = new int[N];
+  static void solve() { }
 
-    for (int i = 0; i < N; i++) {
-      arr[i] = in.nextInt();
+  // Problem 1
+  static void problem1() {
+    int n = ni();
+    int[] arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = ni();
     }
 
-    out.println(Frog.frogOne(N, arr));
-
-    out.close();
-  }
-}
-
-class InputReader {
-  public BufferedReader reader;
-  public StringTokenizer tokenizer;
-
-  public InputReader(InputStream stream) {
-    reader = new BufferedReader(new InputStreamReader(stream), 32768);
-    tokenizer = null;
-  }
-
-  public String next() {
-    while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-      try {
-        tokenizer = new StringTokenizer(reader.readLine());
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-    }
-    return tokenizer.nextToken();
-  }
-
-  public int nextInt() {
-    return Integer.parseInt(next());
-  }
-
-}
-
-class Frog {
-  public static int frogOne(int N, int[] h) {
-    int[] dp = new int[N];
-
-    Arrays.fill(dp, Integer.MAX_VALUE);
+    int[] dp = new int[n];
     dp[0] = 0;
+    dp[1] = Math.abs(arr[1] - arr[0]);
 
-    for (int i = 0; i < N; i++) {
-      for (int j : new int[]{i + 1, i + 2}) {
-        if (j < N) {
-          dp[j] = Math.min(dp[j], dp[i] + Math.abs(h[i] - h[j]));
-        }
-      }
+    for (int i = 2; i < n ; i++) {
+      dp[i] = Math.min(
+          dp[i - 2] + Math.abs(arr[i] - arr[i - 2]),
+          dp[i - 1] + Math.abs(arr[i] - arr[i - 1])
+      );
     }
 
-    return dp[N - 1];
+    out.println(dp[n - 1]);
+  }
+
+  // Problem 2
+  static void problem2() {
+    int n = ni();
+    int k = ni();
+    int[] arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = ni();
+    }
+
+    int[] dp = new int[n];
+    dp[0] = 0;
+    dp[1] = Math.abs(arr[1] - arr[0]);
+
+    for (int i = 1; i < n; i++) {
+      int min = Integer.MAX_VALUE;
+
+      for (int j = 1; j <= k; j++) {
+        min = Math.min(min, dp[i - j] + Math.abs(arr[i] - arr[i - j]));
+
+        if (i - j == 0)
+          break;
+      }
+
+      dp[i] = min;
+    }
+
+    out.println(dp[n - 1]);
+  }
+
+  public static void main(String[] args) throws Exception {
+    long S = System.currentTimeMillis();
+    in = INPUT.isEmpty() ? new Scanner(System.in) : new Scanner(INPUT);
+    out = new PrintWriter(System.out);
+
+    solve();
+    out.flush();
+    long G = System.currentTimeMillis();
+    tr(G - S + "ms");
+  }
+
+  private static int gcd(int a, int b) {
+    while (b != 0) {
+      int t = b;
+      b = a % b;
+      a = t;
+    }
+
+    return a;
+  }
+
+  private static int ni() {
+    return Integer.parseInt(in.next());
+  }
+
+  private static long nl() {
+    return Long.parseLong(in.next());
+  }
+
+  // Math Utils
+  private static int pi(int num, int pow) {
+    int result = 1;
+
+    while (pow > 0) {
+      if (pow % 2 == 1) {
+        result *= num;
+      }
+
+      num *= num;
+      pow /= 2;
+    }
+
+    return result;
+  }
+
+  private static int pim(int num, int pow, int mod) {
+    int result = 1;
+
+    while (pow > 0) {
+      if (pow % 2 == 1) {
+        // keep collecting `odd` num in result
+        result = (result * num) % mod;
+      }
+
+      num = (num * num) % mod;
+      pow /= 2;
+    }
+
+    return result;
+  }
+
+  private static long pl(long num, long pow) {
+    long result = 1;
+
+    while (pow > 0) {
+      if (pow % 2 == 1) {
+        result *= num;
+      }
+
+      num *= num;
+      pow /= 2;
+    }
+
+    return result;
+  }
+
+  private static long plm(long num, long pow, long mod) {
+    long result = 1;
+
+    while (pow > 0) {
+      if (pow % 2 == 1) {
+        // keep collecting `odd` num in result
+        result = (result * num) % mod;
+      }
+
+      num = (num * num) % mod;
+      pow /= 2;
+    }
+
+    return result;
+  }
+
+  static void tr(Object... o) {
+    if (!INPUT.isEmpty())
+      out.println(Arrays.deepToString(o));
   }
 }
