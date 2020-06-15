9f6f7e7b8a528b2230c451a736847c731186e406
Shashi <sbhushan1@outlook.com>
2020-06-16 00:17:11 +0530

    Educational DP Third problem, recursive and iterative solution

--- cp-trials/src/main/java/in/shabhushan/cp_trials/dynamic_programming/EducationalDP.java
+++ cp-trials/src/main/java/in/shabhushan/cp_trials/dynamic_programming/EducationalDP.java
@@ -74,6 +74,70 @@ public class EducationalDP {
     out.println(dp[n - 1]);
   }
 
+  // Problem 3 - Recursive and iterative DP
+  static void problem3() {
+    int n = ni();
+    int[][] arr = new int[n][3];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < 3; j++) {
+        arr[i][j] = ni();
+      }
+    }
+
+//    out.println(Math.max(
+//        helper(arr, n - 2, 0, arr[n-1][0]),
+//        Math.max(
+//            helper(arr, n - 2, 1, arr[n-1][1]),
+//            helper(arr, n - 2, 2, arr[n-1][2])
+//        )
+//    ));
+
+    int[][] dp = new int[n + 1][3];
+
+    // dp[i] = dp[i-1] + max(a[i],b[i],c[i])
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < 3; j++) {
+        // Add arr[i - 1][0] to all the previous 3 dp and check which one is max
+
+        for (int prev = 0; prev < 3; prev++) {
+          if (j == prev) continue; // if prev is same, don't add
+
+          dp[i][j] = Math.max(dp[i][j], dp[i - 1][prev] + arr[i - 1][j]);
+        }
+      }
+    }
+
+    out.println(Math.max(
+        dp[n][0],
+        Math.max(dp[n][1], dp[n][2])
+    ));
+  }
+
+  static int helper(int[][] arr, int row, int choice, int max) {
+    if (row < 0) {
+      return max;
+    }
+
+    if (choice == 0) {
+      return Math.max(
+          helper(arr, row - 1, 1, max + arr[row][1]),
+          helper(arr, row - 1, 2, max + arr[row][2])
+      );
+    } else if (choice == 1) {
+      return Math.max(
+          helper(arr, row - 1, 0, max + arr[row][0]),
+          helper(arr, row - 1, 2, max + arr[row][2])
+      );
+    } else {
+      return Math.max(
+          helper(arr, row - 1, 0, max + arr[row][0]),
+          helper(arr, row - 1, 1, max + arr[row][1])
+      );
+    }
+  }
+
   public static void main(String[] args) throws Exception {
     long S = System.currentTimeMillis();
     in = INPUT.isEmpty() ? new Scanner(System.in) : new Scanner(INPUT);
