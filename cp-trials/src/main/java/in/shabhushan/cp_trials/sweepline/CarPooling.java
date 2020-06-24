9bc62b8fc304258612aaaccd0e46314674544bac
Shashi <sbhushan1@outlook.com>
2020-06-25 01:33:51 +0530

    Another solution for car pooling added

--- cp-trials/src/main/java/in/shabhushan/cp_trials/sweepline/CarPooling.java
+++ cp-trials/src/main/java/in/shabhushan/cp_trials/sweepline/CarPooling.java
@@ -2,6 +2,10 @@ package in.shabhushan.cp_trials.sweepline;
 
 import java.util.*;
 
+/**
+ * Leetcode solution for
+ * https://leetcode.com/problems/car-pooling/submissions/
+ */
 class CarPooling {
   public boolean carPooling(int[][] trips, int capacity) {
     Arrays.sort(trips, (a, b) -> a[1] - b[1]);
@@ -29,5 +33,31 @@ class CarPooling {
     }
 
     return count <= capacity;
+  }
+
+  public boolean carPooling2(int[][] trips, int capacity) {
+    // ending to capacity queue
+    PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> a.getKey() - b.getKey());
+
+    for (int[] trip: trips) {
+      queue.offer(Map.entry(trip[1], -trip[0]));
+      queue.offer(Map.entry(trip[2], trip[0]));
+    }
+
+    int count = capacity;
+
+    while (!queue.isEmpty()) {
+      Map.Entry<Integer, Integer> entry = queue.poll();
+
+      count += entry.getValue();
+      while (!queue.isEmpty() && queue.peek().getKey().equals(entry.getKey())) {
+        count += (queue.poll().getValue());
+      }
+
+      if (count < 0)
+        return false;
+    }
+
+    return true;
   }
 }
