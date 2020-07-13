4b13aecb5522dc2bcb82ad4b63f8f768db125088
Shashi <sbhushan1@outlook.com>
2020-07-14 00:52:01 +0530

    problem link on tower of Hanoi updated

--- cp-trials/src/main/java/in/shabhushan/cp_trials/stack/TowerofHanoi.java
+++ cp-trials/src/main/java/in/shabhushan/cp_trials/stack/TowerofHanoi.java
@@ -1,5 +1,9 @@
 package in.shabhushan.cp_trials.stack;
 
+/**
+ * check the problem here
+ * https://www.hackerearth.com/blog/developers/tower-hanoi-recursion-game-algorithm-explained/#:~:text=Tower%20of%20Hanoi%20algorithm%20explained,these%20disks%20to%20peg%20B.&text=Now%20to%20solve%20the%20problem,peg%20A%20to%20peg%20B.
+ */
 public class TowerofHanoi {
   public static void towerOfHanoi(int pegs, String begin, String auxiliary, String end) {
     if (pegs == 1) {
