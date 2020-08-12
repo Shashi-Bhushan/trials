f45a90ee62d96737632a293751fce03ab9355f09
Shashi <sbhushan1@outlook.com>
2020-08-12 23:47:06 +0530

    KMP Solution added

--- cp-trials/codechef/kmp.cpp
+++ cp-trials/codechef/kmp.cpp
@@ -6,10 +6,13 @@ using namespace std;
 void getLps(string pattern, int *lps) {
 	int size = pattern.size();
 
+	// first is always 0
 	lps[0] = 0;
 
-	int i = 1;
 	int val = 0;
+	// start filling lps from position 1
+	int i = 1;
+
 	while (i < size) {
 		if (pattern[i] == pattern[val]) {
 			val++;
@@ -19,6 +22,7 @@ void getLps(string pattern, int *lps) {
 			if (val != 0) {
 				val = lps[val - 1];
 			} else {
+				// val is 0
 				lps[i] = 0;
 				i++;
 			}
@@ -35,6 +39,26 @@ bool kmp(string str, string pattern) {
 		cout << lps[i] << " ";
 	}
 	cout << endl;
+
+	int i = 0; // string index
+	int j = 0; // pattern index
+
+	while	(i < str.size()) {
+		if (pattern[j] == str[i]) {
+			j++;
+			i++;
+		}
+		
+		if (j == pattern.size()) {
+			cout << "Found pattern from index " << (i - j) << "(inclusive) to index " << i << "(exclusive)." << endl;
+			return true;
+		}
+		else if (i < str.size() && pattern[j] != str[i]) {
+			// mismatch after j matches
+			if (j == 0) i++;
+			else j = lps[j - 1];
+		}
+	}
 
 	return false;
 }
