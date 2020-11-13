cbaaab35f9cdbb37e64e85e631be69d33e3dfe57
Shashi <sbhushan1@outlook.com>
2020-11-13 18:45:55 +0530

    Rod cutting with List of cutting places

--- algo-trials/src/main/java/in/shabhushan/algo_trials/clrs/chapter15/RodCutting.java
+++ algo-trials/src/main/java/in/shabhushan/algo_trials/clrs/chapter15/RodCutting.java
@@ -1,5 +1,9 @@
 package in.shabhushan.algo_trials.clrs.chapter15;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class RodCutting {
   public static int rodCutting(int[] price) {
     int n = price.length;
@@ -23,5 +27,51 @@ public class RodCutting {
     }
 
     return dp[n];
+  }
+
+  public static Map.Entry<Integer, List<Integer>> rodCuttingWithSizes(int[] price) {
+    int n = price.length;
+
+    int[] dp = new int[n + 1];
+    // s is index, where last cut was made for this size
+    int[] s = new int[n + 1];
+
+    dp[0] = 0;
+    s[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+      int maxCutPrice = -1;
+
+      int initialIndex = i - 1;
+
+      for (int j = 0; j < i; j++) {
+        int currentPrice = price[j] + dp[initialIndex - j];
+
+        if (currentPrice > maxCutPrice) {
+          maxCutPrice = currentPrice;
+          s[i] = j + 1; // corresponding -1 in getRodList() when adding this to list
+        }
+      }
+
+      dp[i] = maxCutPrice;
+    }
+
+    return Map.entry(dp[n], getRodList(s));
+  }
+
+  // s will hold where the last rod was cut
+  private static List<Integer> getRodList(int[] rods) {
+    int n = rods.length - 1;
+
+    List<Integer> rodCuts = new ArrayList<>();
+
+    while (n > 0) {
+      rodCuts.add(rods[n] - 1);
+
+      // go to n - rods[n] position to fetch largest cut made at that position
+      n = n - rods[n];
+    }
+
+    return rodCuts;
   }
 }
--- algo-trials/src/test/java/in/shabhushan/algo_trials/clrs/chapter15/RodCuttingTest.java
+++ algo-trials/src/test/java/in/shabhushan/algo_trials/clrs/chapter15/RodCuttingTest.java
@@ -2,7 +2,11 @@ package in.shabhushan.algo_trials.clrs.chapter15;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static in.shabhushan.algo_trials.clrs.chapter15.RodCutting.rodCutting;
+import static in.shabhushan.algo_trials.clrs.chapter15.RodCutting.rodCuttingWithSizes;
 import static org.junit.Assert.assertEquals;
 
 public class RodCuttingTest {
@@ -11,5 +15,20 @@ public class RodCuttingTest {
     assertEquals(6, rodCutting(new int[]{3, 5}));
     assertEquals(22, rodCutting(new int[]{1, 5, 8, 9, 10, 17, 17, 20}));
     assertEquals(24, rodCutting(new int[]{3, 5, 8, 9, 10, 17, 17, 20}));
+  }
+
+  @Test
+  public void testRodCuttingWithSizes() {
+    Map.Entry<Integer, List<Integer>> answer = rodCuttingWithSizes(new int[]{3, 5});
+    assertEquals(Integer.valueOf(6), answer.getKey());
+    assertEquals(List.of(0, 0), answer.getValue());
+
+    answer = rodCuttingWithSizes(new int[]{1, 5, 8, 9, 10, 17, 17, 20});
+    assertEquals(Integer.valueOf(22), answer.getKey());
+    assertEquals(List.of(1, 5), answer.getValue()); // 5 + 17
+
+    answer = rodCuttingWithSizes(new int[]{3, 5, 8, 9, 10, 17, 17, 20});
+    assertEquals(Integer.valueOf(24), answer.getKey());
+    assertEquals(List.of(0, 0, 0, 0, 0, 0, 0, 0), answer.getValue()); // always making a cut at first position
   }
 }
