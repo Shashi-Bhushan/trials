100b951740fd56b108850985c249bf11d06126c7
Shashi <sbhushan1@outlook.com>
2020-12-20 12:16:05 +0530

    LIS test class renamed

--- algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/LongestIncreasingSubsequenceTest.java
+++ algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/LongestIncreasingSubsequenceTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static in.shabhushan.algo_trials.dynamic_programming.LongestIncreasingSubsequence.lis;
 import static org.junit.Assert.assertEquals;
 
-public class TestLongestIncreasingSubsequence {
+public class LongestIncreasingSubsequenceTest {
   @Test
   public void testLongestIncreasingSubsequence() {
     assertEquals(13, lis(new int[]{5, 4, 11, 1, 16, 8, 4, 5, 23, 34, 45, 12, 98, 15, 14, 16, 45, 34, 67, 65, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 8767, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87, 67, 78, 72, 72, 87}));
