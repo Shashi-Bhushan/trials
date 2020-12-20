6f028abcb74c5881425b5add086fbf42e524adbe
Shashi <sbhushan1@outlook.com>
2020-12-20 15:39:01 +0530

    Coin tower DP solution added

--- algo-trials/src/main/java/in/shabhushan/algo_trials/dynamic_programming/CoinTower.java
+++ algo-trials/src/main/java/in/shabhushan/algo_trials/dynamic_programming/CoinTower.java
@@ -6,46 +6,74 @@ import java.util.*;
  * Whis and Beerus are playing a new game today.
  * They form a tower of N coins and make a move in alternate turns. Beerus plays first.
  * In one step, the player can remove either 1, X, or Y coins from the tower.
- *
+ * <p>
  * The person to make the last move wins the game. Can you find out who wins the game?
  */
 public class CoinTower {
 
-    private static int[] candidates;
-    // Map<Pair<Number of stones left, Player playing>, Winner in scenario>
-    private static Map<Map.Entry<Integer, String>, String> map;
+  private static int[] candidates;
+  // Map<Pair<Number of stones left, Player playing>, Winner in scenario>
+  private static Map<Map.Entry<Integer, String>, String> map;
 
-	public static String findWinner(int n, int x, int y) {
-		//Your code goes here
+  public static String findWinnerDP(int n, int x, int y) {
+    //Your code goes here
 
-        candidates = new int[]{1, x ,y};
+    candidates = new int[]{1, x, y};
 
-        map = new HashMap<>();
+    //map = new HashMap<>();
 
-        return helper(n, "Beerus");
-	}
+    //return helper(n, "Beerus");
 
-    private static String helper(int n, String player) {
-        Map.Entry<Integer, String> entry = new AbstractMap.SimpleEntry<>(n, player);
-        if (map.containsKey(entry)) return map.get(entry);
+    // if dp[i] is true, Beerus is going to win the game
+    boolean[] dp = new boolean[n + 1];
 
-        String opponent = getOpponent(player);
+    dp[0] = false;
+    dp[1] = true;
 
-        if (n == 0) return opponent;
-
-        for (int x: candidates) {
-            if (n - x >= 0 && helper(n - x, opponent).equals(player)) {
-                map.put(entry, player);
-                return player;
-            }
+    for (int i = 2; i <= n; i++) {
+      for (int candidate : candidates) {
+        if (i - candidate >= 0) {
+          if (dp[i - candidate] == false) {
+            dp[i] = true;
+          }
         }
-
-        map.put(entry, opponent);
-        return opponent;
+      }
     }
 
-    private static String getOpponent(String player) {
-        return player.equals("Beerus") ? "Whis" : "Beerus";
+    return dp[n] ? "Beerus" : "Whis";
+  }
+
+  public static String findWinner(int n, int x, int y) {
+    //Your code goes here
+
+    candidates = new int[]{1, x, y};
+
+    map = new HashMap<>();
+
+    return helper(n, "Beerus");
+  }
+
+  private static String helper(int n, String player) {
+    Map.Entry<Integer, String> entry = new AbstractMap.SimpleEntry<>(n, player);
+    if (map.containsKey(entry)) return map.get(entry);
+
+    String opponent = getOpponent(player);
+
+    if (n == 0) return opponent;
+
+    for (int x : candidates) {
+      if (n - x >= 0 && helper(n - x, opponent).equals(player)) {
+        map.put(entry, player);
+        return player;
+      }
     }
+
+    map.put(entry, opponent);
+    return opponent;
+  }
+
+  private static String getOpponent(String player) {
+    return player.equals("Beerus") ? "Whis" : "Beerus";
+  }
 
 }
--- algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/CoinTowerTest.java
+++ algo-trials/src/test/java/in/shabhushan/algo_trials/dynamic_programming/CoinTowerTest.java
@@ -3,6 +3,7 @@ package in.shabhushan.algo_trials.dynamic_programming;
 import org.junit.Test;
 
 import static in.shabhushan.algo_trials.dynamic_programming.CoinTower.findWinner;
+import static in.shabhushan.algo_trials.dynamic_programming.CoinTower.findWinnerDP;
 import static org.junit.Assert.assertEquals;
 
 public class CoinTowerTest {
@@ -11,5 +12,12 @@ public class CoinTowerTest {
     assertEquals("Whis", findWinner(4,2, 3));
     assertEquals("Beerus", findWinner(1, 2, 3));
     assertEquals("Beerus", findWinner(10, 6, 5));
+  }
+
+  @Test
+  public void testCoinTowerDP() {
+    assertEquals("Whis", findWinnerDP(4,2, 3));
+    assertEquals("Beerus", findWinnerDP(1, 2, 3));
+    assertEquals("Beerus", findWinnerDP(10, 6, 5));
   }
 }
